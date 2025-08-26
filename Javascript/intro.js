bddc56216b2b4abb49a205a32d6deba8407d47ce
deepakkumarshinde1 <deepakkumar.shinde1@gmail.com>
2025-08-26 15:18:34 +0530

    DAY-1

---
+++ Javascript/intro.js
@@ -0,0 +1,32 @@
+// object base programming language
+// object => prop and methods
+// js is single thread programming language
+// js is sync in nature ( blocking )
+
+//  desktop => vs code
+//  server => master card
+//  console => angular cli
+//  mobile => uber , netflix
+//  front application => godaddy , flipkart , zomato
+
+// variables
+// loosely pro language
+let _name = "deepak1234#@#$%";
+let mobile = "1234567";
+let a = "1";
+let A = "2";
+let b = "1";
+let c = a + b; // 11 , error, 0
+
+let text = "abc";
+
+console.log(typeof text);
+
+text = 12;
+console.log(typeof text);
+
+text = true;
+console.log(typeof text);
+// +  -  *  /  %
+console.log(c);
+console.log(text);
